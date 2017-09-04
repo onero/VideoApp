75c5b5aa9cc7de6bcec026f4f8a0fed3b75570fc
Adam Hansen <adamlars90@gmail.com>
2017-09-04 17:34:29 +0200

    Refactored repository to generic!

--- VideoAppDAL/Repository/ProfileRepository.cs
+++ VideoAppDAL/Repository/ProfileRepository.cs
@@ -1,53 +1,15 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
 using VideoAppDAL.Entities;
 using VideoAppDAL.Interfaces;
 
 namespace VideoAppDAL.Repository
 {
-    public class ProfileRepository : IRepository<Profile>
+    public class ProfileRepository : Repository<Profile>
     {
-        private readonly IContext _context;
-
-        public ProfileRepository(IContext context)
+        public ProfileRepository(DbContext context) : base(context)
         {
-            _context = context;
-        }
-
-        public Profile Create(Profile entity)
-        {
-            return _context.Profiles.Add(entity).Entity;
-        }
-
-        public IEnumerable<Profile> GetAll()
-        {
-            return new List<Profile>(_context.Profiles);
-        }
-
-        public Profile GetById(int id)
-        {
-            return _context.Profiles.FirstOrDefault(p => p.Id == id);
-        }
-
-        public bool Delete(int id)
-        {
-            var profile = GetById(id);
-            if (profile == null) return false;
-            _context.Profiles.Remove(profile);
-            return true;
-        }
-
-        public Profile Update(Profile entity)
-        {
-            return _context.Profiles.Update(entity).Entity;
-        }
-
-        public void ClearAll()
-        {
-            foreach (var profile in _context.Profiles)
-            {
-                _context.Profiles.Remove(profile);
-            }
         }
     }
 }
